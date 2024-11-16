0ef789e351a61685248f4a2986a0fa8ab1aec144
Bagas Noor <19623186@std.stei.itb.ac.id>
2024-11-16 19:10:43 +0700

    feat: implementing start to acquire user

--- src/Command/start.c
+++ src/Command/start.c
@@ -1,7 +1,49 @@
 #include "../Header/start.h"
+#include "../ADT/array/array.h"
+#include "../ADT/mesinkarakter/mesinkarakter.h"
+#include "../ADT/mesinkata/mesinkata.h"
+#include "../ADT/array/array.c"
+#include "../ADT/mesinkarakter/mesinkarakter.c"
+#include "../ADT/mesinkata/mesinkata.c"
+
+int totalinventory;
+int totaluser;
 
 void start(TabUser *Users){
     MakeEmpty(Users);
+    //STARTWORD("../../save/default.txt", "r");
+    STARTWORD("2.txt", "r");
+    totaluser = atoi(CurrentWord.TabWord);
 
-    STARTWORD("../../save/default.txt", "u");
+    for (int i = 0; i < totaluser; i++){
+        ADVWORD();
+        Users->TC[i].money = atoi(CurrentWord.TabWord);
+
+        ADVWORD();
+        for (int j = 0; j < CurrentWord.Length; j++){
+            Users->TC[i].name[j] = CurrentWord.TabWord[j];
+        }
+        Users->TC[i].name[CurrentWord.Length] = '\0';
+
+        ADVWORD();
+        for (int j = 0; j < CurrentWord.Length; j++){
+            Users->TC[i].password[j] = CurrentWord.TabWord[j];
+        }
+        Users->TC[i].password[CurrentWord.Length] = '\0';
+    }
+    
+    for (int i = 0; i < totaluser; i++)
+    {
+        printf("%s : %d\n", Users->TC[i].name, Users->TC[i].money);
+    }
+    
+}
+    
+
+
+
+
+int main(){
+    TabUser user;
+    start(&user);
 }
