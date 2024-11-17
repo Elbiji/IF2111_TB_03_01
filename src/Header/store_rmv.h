dd2aab121cd17e738025adfb7d7bfef5e12a4d6a
Bagas Noor <19623186@std.stei.itb.ac.id>
2024-11-17 22:38:57 +0700

    feat: Laying foundation for store_rmv

---
+++ src/Header/store_rmv.h
@@ -0,0 +1,12 @@
+#ifndef __STORE_RMV_H__
+#define __STORE_RMV_H__
+
+#include <stdlib.h>
+
+#include "start.h"
+#include "../ADT/array/array.h"
+#include "../ADT/arraydin/arraydin.h"
+#include "../ADT/queue/queue.h"
+#include "../ADT/boolean/boolean.h"
+
+#endif
