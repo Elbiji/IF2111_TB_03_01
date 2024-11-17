9be4fc3179ebddaa60a349debe8f34b2a7c02f15
Bagas Noor <19623186@std.stei.itb.ac.id>
2024-11-17 16:21:07 +0700

    feat: Implementing circular buffer and full functionality of store request

--- src/Command/store_req.c
+++ src/Command/store_req.c
@@ -7,34 +7,121 @@
 ArrayDinBarang array;
 Queue items_request;
 
+int strLength(char *str){
+    int i = 0;
+    while (str[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
+boolean isItemInShop(ArrayDinBarang array, char *str){
+    for (int i = 0; i < array.Neff; i++){
+        boolean exist = true;
+        int j = 0;
+
+        while (array.A[i].name[j] != '\0' && str[j] != '\0'){
+            if (array.A[i].name[j] != str[j]){
+                exist = false;
+                break;
+            }
+            j++;
+        }
+
+        if (exist && array.A[i].name[j] == '\0' && str[j] == '\0'){
+            return true;
+        }
+    }
+    return false;
+}
+
+boolean isItemInQueue(Queue items_request, char *str){
+    for (int i = 0; i < length(items_request); i++){
+        boolean exist = true;
+        int j = 0;
+
+        while (items_request.buffer[i].name[j] != '\0' && str[j] != '\0'){
+            if (items_request.buffer[i].name[j] != str[j]){
+                exist = false;
+                break;
+            }
+            j++;
+        }
+
+        if (exist && items_request.buffer[i].name[j] == '\0' && str[j] == '\0'){
+            return true;
+        }
+    }
+    return false;
+}
+
+boolean isStrAllDigit (char *str){
+    boolean status = true;
+    int i = 0;
+    while (status && i < strLength(str))
+    {
+        if ('0' <= str[i] && str[i] >= '9'){
+            flag = true;
+        } else {
+            status = false;
+        }
+        i++;
+    }
+    return status;
+}
+
 void store_request(Queue *items_request, ArrayDinBarang array){
     char requested_item[50];
 
-    
-    printf("Nama barang yang diminta: ");
-    scanf("%49s", requested_item);
+    if (isFull(items_request)){
+        printf("antrian penuh");
+        return;
+    } else {
+        printf("======= [STORE REQUEST] =======");
+        while(1){
+            printf("Nama barang yang diminta: ");
+            scanf("%49s", requested_item);
+            if (isStrAllDigit(*requested_item)){
+                printf("Pastikan anda memasukkan nama berupa karakter\n");
+            } else if (isItemInShop(array, *requested_item)){
+                printf("Barang sudah ada di toko!\n")
+            } else {
+                if (isItemInQueue(items_request, *requested_item))
+                {
+                    printf("Barang sudah berada di dalam antrian!\n");
+                } else {
+                    enqueue(items_request, requested_item);
+                    printf("Barang berhasil dimasukkan ke dalam antrian\n");
+                    break;
+                }   
+            }   
+        }   
+    }
 }
 
-// int main() {
-//     TabUser user;
-//     ArrayDinBarang items;
-//     Queue item_req:
-//     MakeArrayDinBarang(&items);
-//     start(&user, &items, &item_req);
+int main() {
+    TabUser user;
+    ArrayDinBarang items;
+    Queue item_req:
+    MakeArrayDinBarang(&items);
+    start(&user, &items, &item_req);
     
-//     // Print results to verify
-//     printf("\nLoaded Users:\n");
-//     for (int i = 0; i <  user.Neff; i++) {
-//         printf("User %d: %s (Money: %d)\n", 
-//                i+1, user.TC[i].name, user.TC[i].money);
-//     }
+    // Print results to verify
+    printf("\nLoaded Users:\n");
+    for (int i = 0; i <  user.Neff; i++) {
+        printf("User %d: %s (Money: %d)\n", 
+               i+1, user.TC[i].name, user.TC[i].money);
+    }
     
-//     printf("\nLoaded Items:\n");
-//     for (int i = 0; i < items.Neff; i++) {
-//         printf("Item %d: %s (Price: %d)\n", 
-//                i+1, items.A[i].name, items.A[i].price);
-//     }
+    printf("\nLoaded Items:\n");
+    for (int i = 0; i < items.Neff; i++) {
+        printf("Item %d: %s (Price: %d)\n", 
+               i+1, items.A[i].name, items.A[i].price);
+    }
     
-//     DeallocateArrayDinBarang(&items);
-//     return 0;
-// }
+    store_request(&item_req, items);
+    displayQueue(item_req);
+
+    DeallocateArrayDinBarang(&items);
+    return 0;
+}
