f92f882797650aea8c2535c42623b9c3ba3687c9
Bagas Noor <19623186@std.stei.itb.ac.id>
2024-11-21 00:22:56 +0700

    fix: small fix in logout (Aulia's code) to adjust current state of program

--- src/Command/logout.c
+++ src/Command/logout.c
@@ -2,11 +2,12 @@
 #include "../Header/logout.h"
 
 // Fungsi Logout
-void logout(boolean *isLogged, TabUser list, IdxType iduser) {
-    if (isLogged == false) {
+void logout(boolean *isLogged, TabUser list, IdxType *iduser) {
+    if (*isLogged == false) {
         printf("Anda belum login.\n");
     } else {
         printf("%s telah logout dari sistem PURRMART. Silakan REGISTER/LOGIN kembali untuk melanjutkan.\n", list.TC[iduser].name);
-        isLogged = false;
+        *isLogged = false;
+        *iduser = IdxUndef;
     }
 }
