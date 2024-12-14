8c8c822c289b27efd6f730085dc6d0ce1de17977
AuliaAzkaA <19623257@std.stei.itb.sc.id>
2024-12-14 22:03:58 +0700

    feat: Add fiture profile

--- src/Command/profile.c
+++ src/Command/profile.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "../Header/profile.h"
 
+// Fungsi Profile
+void profile(boolean isLogged, TabUser list, IdxType iduser) {
+    if (isLogged == false) {
+        printf("Anda belum login.\n");
+    } else {
+        printf("Nama: %s\n", list.TC[iduser].name);
+        printf("Username: %s\n", list.TC[iduser].name);
+        printf("Saldo: %d\n", list.TC[iduser].money);
+    }
+}
---
+++ src/Header/profile.h
@@ -0,0 +1,11 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+#include "../ADT/boolean/boolean.h"
+#include "../../misc.h"
+#include "../ADT/array/array.h"
+
+// Deklarasi fungsi profile
+void profile(boolean isLogged, TabUser list, IdxType iduser) ;
+
+#endif
