7cae392b0d1967a50a9515f3e39987f67c2094d6
Bagas Noor <19623186@std.stei.itb.ac.id>
2024-12-18 12:19:42 +0700

    fix: revised inline handling for items that has multiple words in cart add and cart remove

--- src/Command/cart_add.c
+++ src/Command/cart_add.c
@@ -4,6 +4,7 @@ boolean isInputCartAdd(char *input, int *amount, char *nama_barang){
     const char *command = "CART ADD";
     int j;
     int i = 0;
+    int last_space = -1;
 
     while (command[i] != '\0'){
         if (input[i] != command[i]){
@@ -16,23 +17,36 @@ boolean isInputCartAdd(char *input, int *amount, char *nama_barang){
         i++;
     }
 
-   j = 0;
-    while (input[i] != ' ' && input[i] != '\0'){
-        nama_barang[j++] = input[i++];
-    }
-    nama_barang[j] = '\0';
-
-    while (input[i] == ' '){
-        i++;
+    int current = i;
+    while(input[current] != '\0'){
+        if (input[current] == ' '){
+            last_space = current;
+        }
+        current++;
     }
 
     char num1[10];
+    int amount_start = last_space + 1;
     j = 0;
-    while (input[i] >= '0' && input[i] <= '9' && j < 9){
-        num1[j++] = input[i++];
+    while (input[amount_start] >= '0' && input[amount_start] <= '9' && j < 9){
+        num1[j++] = input[amount_start++];
     }
     num1[j] = '\0';
     *amount = atoi(num1);
+
+    int name_start = i;
+    int name_end = last_space - 1;
+
+    while (name_end >= name_start && input[name_end] == ' '){
+        name_end--;
+    }
+
+    int name_length = name_end - name_start + 1; // Add + 1 so counts both start and end
+    j = 0; 
+    while(j < name_length && j < (MAX_LEN-1)){
+        nama_barang[j++] = input[name_start++];
+    }
+    nama_barang[j] = '\0';
 
     if (*amount > 0){
         return true;
